3bd716eaac4371fcaf2ea7da3783fdbc92e8b481
wuhongjing <wuhongjing88@126.com>
2016-09-09 11:50:36 +0800

    version 2.o

--- JS封装/removeEvent.js
+++
@@ -1,9 +0,0 @@
-// 事件解除绑定的兼容封装
-'use strict'
-function removeEvent(obj,sEv,fn){
-	if(obj.removeEventListener){
-		obj.removeEventListener(sEv,fn,false);
-	}else{
-		obj.detachEvent('on'+sEv,fn);
-	};
-};
